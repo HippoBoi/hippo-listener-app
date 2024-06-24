7998c9598986d1f17089782d32173fd1d3902fbd
HippoBoi <tagrifavc@gmail.com>
2024-06-23 22:37:20 -0400

    play loaded audio

--- lib/pages/upload_song.dart
+++ lib/pages/upload_song.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class UploadSong extends StatefulWidget {
   const UploadSong({super.key});
@@ -10,18 +12,32 @@ class UploadSong extends StatefulWidget {
 }
 
 class UploadSongState extends State<UploadSong> {
-  File? _file;
+  String? _fileURL;
+  final AudioPlayer _audioPlayer = AudioPlayer();
 
   Future<void> _pickFile() async {
-    // ignore: avoid_print
-    print("tamo aqui 1");
     FilePickerResult? result = await FilePicker.platform.pickFiles(
         type: FileType.custom, allowedExtensions: ["mp3", "ogg", "wav"]);
 
-    if (result != null && result.files.single.path != null) {
+    if (result != null) {
+      File file = File(result.files.single.path!);
+      Directory directory = await getApplicationDocumentsDirectory();
+      String directoryPath = directory.path;
+      final localFile = File("$directoryPath/${file.uri.pathSegments.last}");
+      await file.copy(localFile.path);
+
       setState(() {
-        _file = File(result.files.single.path!);
+        _fileURL = localFile.path;
       });
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("Archivo guardado")));
+    }
+  }
+
+  void playAudio() async {
+    if (_fileURL != null) {
+      await _audioPlayer.play(UrlSource(_fileURL!));
     }
   }
 
@@ -43,7 +59,9 @@ class UploadSongState extends State<UploadSong> {
             const SizedBox(height: 20),
             ElevatedButton(
                 onPressed: _pickFile, child: const Text("Sube una canción")),
-            if (_file != null) Text("Archivo: ${_file!.path.split("/").last}")
+            if (_fileURL != null)
+              ElevatedButton(
+                  onPressed: playAudio, child: const Icon(Icons.play_arrow)),
           ],
         ),
       ),
